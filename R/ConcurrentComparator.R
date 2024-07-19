cc7b63113c714ef2d96c613c8c4366503a7fa92b
shounakch <shounak.chattopadhyay@gmail.com>
2024-07-19 04:00:30 -0400

    added LLR calculation

--- R/ConcurrentComparator.R
+++ R/ConcurrentComparator.R
@@ -24,8 +24,12 @@ clusterFunction <- function(outcomeId,
   exposureId = settings$exposureId
   baseExposureId = settings$baseExposureId
   dataName = settings$dataName
+  outputFolder = settings$outputFolder
+  tPeriod = settings$tPeriod
   
-  loadDir = paste0("E:/eumaeusTest_", dataName, "_Shounak/ConcurrentComparator/e_", baseExposureId, "/ccData_e_", exposureId, "_t", analysisId, ".zip")
+  #loadDir = paste0("E:/eumaeusTest_", dataName, "_Shounak/ConcurrentComparator/e_", baseExposureId, "/ccData_e_", exposureId, "_t", analysisId, ".zip")
+  
+  loadDir = paste0(outputFolder, "/ccData_e_", exposureId, "_t", tPeriod, ".zip")
   
   ccDataObject = Andromeda::loadAndromeda(fileName = loadDir)
   class(ccDataObject) <- "ConcurrentComparatorData"
@@ -33,64 +37,64 @@ clusterFunction <- function(outcomeId,
   
   population <- ConcurrentComparator:::createStudyPopulation(ccDataObject, outcomeId = outcomeId)
   
-  #View(population)
-  
-  #t4 = proc.time()
-  
   fit <- ConcurrentComparator:::fitOutcomeModel(population = population)
   
-  #t5 = proc.time()
+  # Evaluate llr
   
-  # if((sum(fit$outcomeStatistics$outcomes) == 0) || (fit$status == "ILLCONDITIONED")) {
-  #   
-  #   fitLogRr = NA
-  #   fitLogLb95 = NA
-  #   fitLogUb95 = NA
-  #   fitSeLogRr = NA
-  #   
-  # } else {
-  #   
-  #   fitLogRr = fit$treatmentEstimate$logRr
-  #   fitLogLb95 = fit$treatmentEstimate$logLb95
-  #   fitLogUb95 = fit$treatmentEstimate$logUb95
-  #   fitSeLogRr = fit$treatmentEstimate$seLogRr
-  #   
-  # }
+  fit <- ConcurrentComparator::fitOutcomeModel(population = population,
+                                               profileGrid = c(as.numeric(fit$coefficients), 0),
+                                               profileBounds = NULL)
   
-  output = c(exposureId,
-             baseExposureId,
-             outcomeId,
-             analysisId,
-             fit$outcomeStatistics$subjects[2],
-             fit$outcomeStatistics$outcomes[2],
-             fit$outcomeStatistics$kPtYrs[2],
-             fit$outcomeStatistics$subjects[1],
-             fit$outcomeStatistics$outcomes[1],
-             fit$outcomeStatistics$kPtYrs[1],
-             fit$treatmentEstimate$logRr,
-             fit$treatmentEstimate$logLb95,
-             fit$treatmentEstimate$logUb95,
-             fit$treatmentEstimate$seLogRr)
+  baseOutput = c(exposureId,
+                 baseExposureId,
+                 outcomeId,
+                 analysisId,
+                 fit$outcomeStatistics$subjects[2],
+                 fit$outcomeStatistics$outcomes[2],
+                 fit$outcomeStatistics$kPtYrs[2],
+                 fit$outcomeStatistics$subjects[1],
+                 fit$outcomeStatistics$outcomes[1],
+                 fit$outcomeStatistics$kPtYrs[1])
+  
+  # output = c(exposureId,
+  #            baseExposureId,
+  #            outcomeId,
+  #            analysisId,
+  #            fit$outcomeStatistics$subjects[2],
+  #            fit$outcomeStatistics$outcomes[2],
+  #            fit$outcomeStatistics$kPtYrs[2],
+  #            fit$outcomeStatistics$subjects[1],
+  #            fit$outcomeStatistics$outcomes[1],
+  #            fit$outcomeStatistics$kPtYrs[1],
+  #            fit$treatmentEstimate$logRr,
+  #            fit$treatmentEstimate$logLb95,
+  #            fit$treatmentEstimate$logUb95,
+  #            fit$treatmentEstimate$seLogRr)
   
   if((sum(fit$outcomeStatistics$outcomes) == 0) || (fit$status != "OK")) {
     
-    output = c(output, rep(NA, 4))
+    output = c(baseOutput, rep(NA, 5))
+    
+  } else {
+    
+    if(fit$treatmentEstimate$logRr <= 0) {
+      
+      llr = 1e-6
+      
+    } else {
+      
+      llr = fit$logLikelihoodProfile$value[1] - fit$logLikelihoodProfile$value[2]
+      
+    }
+    
+    output = c(baseOutput, 
+               fit$treatmentEstimate$logRr,
+               fit$treatmentEstimate$logLb95,
+               fit$treatmentEstimate$logUb95,
+               fit$treatmentEstimate$seLogRr,
+               llr)
     
   }
-  
-  # outputDf = rbind(outputDf, c(exposureId,
-  #                              outcomeId,
-  #                              analysisId,
-  #                              fit$outcomeStatistics$subjects[2],
-  #                              fit$outcomeStatistics$outcomes[2],
-  #                              fit$outcomeStatistics$kPtYrs[2],
-  #                              fit$outcomeStatistics$subjects[1],
-  #                              fit$outcomeStatistics$outcomes[1],
-  #                              fit$outcomeStatistics$kPtYrs[1],
-  #                              fitLogRr,
-  #                              fitLogLb95,
-  #                              fitLogUb95,
-  #                              fitSeLogRr))
   
   return(output)
   
@@ -112,7 +116,8 @@ runConcurrentComparator <- function(connectionDetails,
     dir.create(concurrentComparatorFolder)
   
   concurrentComparatorSummaryFile <- file.path(outputFolder, "ConcurrentComparatorSummary.csv")
-  if (!file.exists(concurrentComparatorSummaryFile)) {
+  #if (!file.exists(concurrentComparatorSummaryFile)) {
+  if(TRUE) {
     
     allControls <- Eumaeus:::loadAllControls(outputFolder)
     
@@ -184,8 +189,9 @@ runConcurrentComparator <- function(connectionDetails,
         # periodEstimates <- bind_rows(periodEstimates)
         # readr::write_csv(periodEstimates, periodEstimatesFile)
         
-        if (!file.exists(periodEstimatesFile)) {
+        #if (!file.exists(periodEstimatesFile)) {
         #if (!file.exists(periodEstimatesFile) || (length(exposures$exposureId) > 1)) {
+        if(TRUE) {
 
           periodEstimates <- list()
           #exposureId <- exposures$exposureId[1]
@@ -202,11 +208,12 @@ runConcurrentComparator <- function(connectionDetails,
                                                               endDate = as.character(timePeriods$endDate[i]),
                                                               exposureId = exposureId,
                                                               outcomeIds = controls$outcomeId,
-                                                              analysisId = i,
+                                                              analysisId = 2, #analysisId = 2 for 1-21 days vs 22-42 days
                                                               outputFolder = exposureFolder,
                                                               cluster = cluster,
                                                               dataName = dataName,
-                                                              baseExposureId = baseExposureId)
+                                                              baseExposureId = baseExposureId,
+                                                              tPeriod = i)
             periodEstimates[[length(periodEstimates) + 1]] <- estimates
             
           }
@@ -261,38 +268,10 @@ computeConcurrentComparatorEstimates <- function(connectionDetails,
                                                  outputFolder,
                                                  cluster,
                                                  dataName,
-                                                 baseExposureId) {
+                                                 baseExposureId,
+                                                 tPeriod) {
   
   start <- Sys.time()
-  # analysisList <- list(
-  #   ConcurrentComparator:::createConcurrentComparatorAnalysis(analysisId = i,
-  #                                                             studyStartDate = startDate[1],
-  #                                                             #studyEndDate = "2021-06-30",
-  #                                                             studyEndDate = endDate[1],
-  #                                                             timeAtRiskStart = 1,
-  #                                                             timeAtRiskEnd = 21,
-  #                                                             washoutTime = 22))
-  # # )
-  # # ConcurrentComparator::createConcurrentComparatorAnalysis(analysisId = 2,
-  # #                                                          studyEndDate = "2021-06-30",
-  # #                                                          timeAtRiskStart = 0,
-  # #                                                          timeAtRiskEnd = 7, # WORKING? NEED TEST
-  # #                                                          washoutTime = 36)  # WORKING? NEED TEST
-  # # )
-  # 
-  # results <- ConcurrentComparator:::runConcurrentComparatorAnalyses(connectionDetails = connectionDetails,
-  #                                                                   cdmDatabaseSchema = cdmDatabaseSchema,
-  #                                                                   exposureDatabaseSchema = cohortDatabaseSchema,
-  #                                                                   exposureTable = cohortTable,
-  #                                                                   outcomeDatabaseSchema = cohortDatabaseSchema,
-  #                                                                   outcomeTable = cohortTable,
-  #                                                                   outputFolder = outputFolder,
-  #                                                                   analysisList = analysisList,
-  #                                                                   targetIds = exposureId,
-  #                                                                   controlIds = outcomeIds,
-  #                                                                   outcomeIds = c(668))
-  
-  #t1 = proc.time()
   
   ccData <- ConcurrentComparator:::getDbConcurrentComparatorData(connectionDetails = connectionDetails,
                                                                  cdmDatabaseSchema = cdmDatabaseSchema,
@@ -311,32 +290,20 @@ computeConcurrentComparatorEstimates <- function(connectionDetails,
                                                                  timeAtRiskEnd = 21,
                                                                  washoutTime = 22)
   
-  saveDir = paste0("E:/eumaeusTest_", dataName, "_Shounak/ConcurrentComparator/e_", baseExposureId, "/ccData_e_", exposureId, "_t", analysisId, ".zip")
+  #saveDir = paste0("E:/eumaeusTest_", dataName, "_Shounak/ConcurrentComparator/e_", baseExposureId, "/ccData_e_", exposureId, "_t", analysisId, ".zip")
+  
+  saveDir = paste0(outputFolder, "/ccData_e_", exposureId, "_t", tPeriod, ".zip")
   
   #Andromeda::saveAndromeda(ccData, fileName = paste0("E:/eumaeusTest_optum_ehr_Shounak/ConcurrentComparator/e_", exposureId, "/ccData_t", analysisId, ".zip"), maintainConnection = TRUE)
   Andromeda::saveAndromeda(ccData, fileName = saveDir, maintainConnection = TRUE)
   
-  #t2 = proc.time()
-  
-  # outputDf = data.frame(exposureId = c(0),
-  #                       outcomeId = c(0),
-  #                       analysisId = c(0),
-  #                       targetSubjects = c(0),
-  #                       targetOutcomes = c(0),
-  #                       targetYears = c(0),
-  #                       comparatorSubjects = c(0),
-  #                       comparatorOutcomes = c(0),
-  #                       comparatorYears = c(0),
-  #                       logRr = c(0),
-  #                       logLb95 = c(0),
-  #                       logUb95 = c(0),
-  #                       seLogRr = c(0))
-  
-  settings = list("ccDataName" = paste0("ccData_e_", baseExposureId, "_t", analysisId),
+  settings = list("ccDataName" = paste0("ccData_e_", exposureId, "_t", tPeriod),
                   "analysisId" = analysisId,
                   "exposureId" = exposureId,
                   "baseExposureId" = baseExposureId,
-                  "dataName" = dataName)
+                  "dataName" = dataName,
+                  "tPeriod" = tPeriod,
+                  "outputFolder" = outputFolder)
   
   estimates <- ParallelLogger::clusterApply(cluster, outcomeIds, clusterFunction, 
                                             settings = settings)
@@ -355,59 +322,10 @@ computeConcurrentComparatorEstimates <- function(connectionDetails,
                           "logRr",
                           "logLb95",
                           "logUb95",
-                          "seLogRr")
+                          "seLogRr",
+                          "llr")
   
   estimates = as.data.frame(estimates)
-  
-  # for(outcomeId in outcomeIds) {
-  #   
-  #   t3 = proc.time()
-  #   
-  #   population <- ConcurrentComparator:::createStudyPopulation(ccData, outcomeId = outcomeId)
-  #   #View(population)
-  #   
-  #   t4 = proc.time()
-  #   
-  #   fit <- ConcurrentComparator:::fitOutcomeModel(population = population)
-  #   
-  #   t5 = proc.time()
-  #   
-  #   if(sum(fit$outcomeStatistics$outcomes) == 0) {
-  #     
-  #     fitLogRr = NA
-  #     fitLogLb95 = NA
-  #     fitLogUb95 = NA
-  #     fitSeLogRr = NA
-  #      
-  #   } else {
-  #     
-  #     fitLogRr = fit$treatmentEstimate$logRr
-  #     fitLogLb95 = fit$treatmentEstimate$logLb95
-  #     fitLogUb95 = fit$treatmentEstimate$logUb95
-  #     fitSeLogRr = fit$treatmentEstimate$seLogRr
-  #     
-  #   }
-  #   
-  #   outputDf = rbind(outputDf, c(exposureId,
-  #                                outcomeId,
-  #                                analysisId,
-  #                                fit$outcomeStatistics$subjects[2],
-  #                                fit$outcomeStatistics$outcomes[2],
-  #                                fit$outcomeStatistics$kPtYrs[2],
-  #                                fit$outcomeStatistics$subjects[1],
-  #                                fit$outcomeStatistics$outcomes[1],
-  #                                fit$outcomeStatistics$kPtYrs[1],
-  #                                fitLogRr,
-  #                                fitLogLb95,
-  #                                fitLogUb95,
-  #                                fitSeLogRr))
-  #   
-  # }
-  # 
-  # outputDf = outputDf[-1,]
-  
-  #controlIds = c(74816)) - how to incorporate this?
-  # aggreagateConcurrentComparatorResults(results, outputFolder)
   
   delta <- Sys.time() - start
   message(paste("Computing concurrent comparator estimates took", signif(delta, 3), attr(delta, "units")))
